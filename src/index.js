7d73ee5ba28d166ea1d1cfdb0ea0f667c2873bd1
Domingo Cabeza <domingoacd1@gmail.com>
2019-07-12 14:51:18 -0400

    Comenzada estructura base del card con React

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
+class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,27 +10,20 @@ class Clock extends React.Component {
     };
   }
   
-  startTimer() {
-    this.setState({
-      date: new Date()
-    });
-  }
-  componentDidMount() {
-    this.timerId = setInterval(() => this.startTimer(), 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
   render() {
     return (
-      <h1>Time is: {this.state.date.toLocaleTimeString()}</h1>     
+      <section className="main-container">
+        <Character-image />
+        <Character-info />
+        <Menu-side />
+      </section>   
     );
   }
 }
 
 function timer() {
   ReactDOM.render(
-    <Clock />,
+    <Card />,
     document.getElementById('root')
   );
 }
