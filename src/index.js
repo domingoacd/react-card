4bae570b0f3f8a543f3a32c690219192ce71e15b
Domingo Cabeza <domingoacd1@gmail.com>
2019-07-29 21:19:51 -0400

    Developing functionality to change character on click

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ class CharacterSelection extends React.Component {
     super(props);
     this.state = {
       currentSelectedCharacter : 0,
+      characters : props.characters,
       amountOfCharacters : props.characters.length - 1
     };
     
@@ -125,7 +126,7 @@ class CharacterSelection extends React.Component {
         </button>
         <div className="image_slide">
           <div className="image_slide__inner">
-            <img className="thumb thumb--1 active" src={superman} alt="" />
+            <img className="thumb thumb--1 active" src={superman} alt="" onClick={this.props.manageClick}/>
             <img className="thumb thumb--2" src={spiderman} alt="" />
             <img className="thumb thumb--3" src={wolverine} alt="" />
             <img className="thumb thumb--1" src={superman} alt="" />
@@ -188,25 +189,92 @@ class Card extends React.Component {
     super(props);
     this.state = {
       charactersList: [
-        "superman",
-        "spiderman",
-        "wolverine",
-        "superman",
-        "spiderman",
-        "wolverine",
-        "superman",
-        "spiderman"
+        {
+          code: "000",
+          image: superman,
+          title: "Man of Steel",
+          name: "Kal El",
+          comic_house: "DC",
+          info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, sem porta 
+                interdum fringilla, diam massa rutrum nibh, id condimentum purus urna nec turpis.`,
+          biography: "https://www.google.com",
+          enemies: "https://www.google.com",
+          partnerships: "https://www.google.com"
+        },
+        {
+          code: "001",
+          image: spiderman,
+          title: "Amazing Spider-Man",
+          name: "Peter Parker",
+          comic_house: "MARVEL",
+          info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, sem porta 
+                interdum fringilla, diam massa rutrum nibh, id condimentum purus urna nec turpis.`,
+          biography: "https://www.facebook.com",
+          enemies: "https://www.facebook.com",
+          partnerships: "https://www.facebook.com"
+        },
+        {
+          code: "002",
+          image: wolverine,
+          title: "Weapon X Logan",
+          name: "Logan",
+          comic_house: "MARVEL",
+          info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, sem porta 
+                interdum fringilla, diam massa rutrum nibh, id condimentum purus urna nec turpis.`,
+          biography: "https://www.twitter.com",
+          enemies: "https://www.twitter.com",
+          partnerships: "https://www.twitter.com"
+        },
+        {
+          code: "003",
+          image: superman,
+          title: "Man of Steel",
+          name: "Kal El",
+          comic_house: "DC",
+          info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, sem porta 
+                interdum fringilla, diam massa rutrum nibh, id condimentum purus urna nec turpis.`,
+          biography: "https://www.google.com",
+          enemies: "https://www.google.com",
+          partnerships: "https://www.google.com"
+        },
+        {
+          code: "004",
+          image: spiderman,
+          title: "Amazing Spider-Man",
+          name: "Peter Parker",
+          comic_house: "MARVEL",
+          info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, sem porta 
+                interdum fringilla, diam massa rutrum nibh, id condimentum purus urna nec turpis.`,
+          biography: "https://www.facebook.com",
+          enemies: "https://www.facebook.com",
+          partnerships: "https://www.facebook.com"
+        },
+        {
+          code: "005",
+          image: wolverine,
+          title: "Weapon X Logan",
+          name: "Logan",
+          comic_house: "MARVEL",
+          info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, sem porta 
+                interdum fringilla, diam massa rutrum nibh, id condimentum purus urna nec turpis.`,
+          biography: "https://www.twitter.com",
+          enemies: "https://www.twitter.com",
+          partnerships: "https://www.twitter.com"
+        },
       ]
     };
   }
   
+  changeCharacter(event) {
+    console.log("hey->", event.currentTarget);
+  }
 
   render() {
     return (
       <section className="main-container">
         <CharacterImage />
         <CharacterInfo />
-        <CharacterSelection characters={this.state.charactersList} />
+        <CharacterSelection characters={this.state.charactersList} manageClick={this.changeCharacter}/>
         <MenuSide />
       </section>   
     );
