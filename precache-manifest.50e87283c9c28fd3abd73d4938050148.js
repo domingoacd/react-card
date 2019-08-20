5243a7a6f4a38b6893d05d065b91a65e6e3ff624
Domingo Cabeza <domingoacd1@gmail.com>
2019-08-20 15:10:50 -0400

    Updates

---
+++ precache-manifest.50e87283c9c28fd3abd73d4938050148.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138df6a8660df33749e23aca0aa715c6",
+    "url": "/react-card/index.html"
+  },
+  {
+    "revision": "cce082123e88cb41dbd9",
+    "url": "/react-card/static/css/main.e6c203a5.chunk.css"
+  },
+  {
+    "revision": "f20982cbc385378c01ff",
+    "url": "/react-card/static/js/2.da95ac23.chunk.js"
+  },
+  {
+    "revision": "cce082123e88cb41dbd9",
+    "url": "/react-card/static/js/main.38074728.chunk.js"
+  },
+  {
+    "revision": "13008e0c359b1a8d0c85",
+    "url": "/react-card/static/js/runtime~main.683265bf.js"
+  },
+  {
+    "revision": "32b0daa65320d2e10dc04947ddc24817",
+    "url": "/react-card/static/media/instagram-logo.32b0daa6.png"
+  },
+  {
+    "revision": "737807148b54fcae4d95b7d5c00594a3",
+    "url": "/react-card/static/media/menu.73780714.png"
+  },
+  {
+    "revision": "7c123aaf21bb2a6feb3e527cbed6e03f",
+    "url": "/react-card/static/media/spiderman.7c123aaf.png"
+  },
+  {
+    "revision": "db135ff1ea3f98783a2184492f460a52",
+    "url": "/react-card/static/media/superman.db135ff1.png"
+  },
+  {
+    "revision": "02a68eeb4e80d9770bbe32c7a0065e06",
+    "url": "/react-card/static/media/wolverine.02a68eeb.png"
+  }
+]);
